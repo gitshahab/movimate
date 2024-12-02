2492b17887659fbddf5ba2f211b3ebbe070b8ef5
Mohd Shahab <mohdshahab9520@gmail.com>
2024-12-03 01:30:18 +0530

    add movi-o url

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -10,7 +10,7 @@ export const MovieList = ({apiPath, pageNum, setPageNum, title}) => {
     useEffect(() => {
         document.title = `${title}/Movimate`;
     });
-
+    
     const handlePageChange = () => {
         setPageNum(pageNum + 1);  
     };
@@ -20,6 +20,7 @@ export const MovieList = ({apiPath, pageNum, setPageNum, title}) => {
             <div className='flex flex-col justify-center items-center h-screen p-4'>
                 <h1 className='font-medium text-2xl text-gray-800 dark:text-gray-400'>Oops! The TMDB API is currently unavailable.</h1>
                 <h3 className='font-medium text-xl text-gray-800 dark:text-gray-400'>Please try again later.</h3>
+                <span className='text-xl text-gray-800 dark:text-gray-400 py-2'>Check out movi-o, based on OMDB API.<a className='text-blue-600' href="https://movi-o.netlify.app" target="_blank" rel="noopener noreferrer"> Visit movi-o</a></span>
             </div>
         )
         
